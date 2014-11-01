353f672d22066af0aeb8fb840e9279424ac2f3bc
aronb12 <arona12@ru.is>
2014-11-01 22:27:40 +0000

    Frontend madness! FRONTEND SPARTA!!!!

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,8 @@
+import json
+#import util
+import urllib
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -6,6 +10,19 @@ app = Flask(__name__)
 def home():
     welcome = "Hello World"
     return render_template("home.html", welcome=welcome)
+
+@app.route('/functions/')
+@app.route('/functions')
+def functions():
+    url = 'http://localhost:5000/kodemon/all'
+    data = json.load(urllib.urlopen(url))["result"]
+    return render_template('functions.html', list=data)
+
+@app.route('/functions/<name>')
+def function(name):
+    url = 'http://localhost:5000/kodemon/function/' + name
+    data = json.load(urllib.urlopen(url))['result'][0]
+    return render_template('function.html', data=data)
 
 
 if __name__ == '__main__':
